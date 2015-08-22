6bc51281a734e2828f9de16b7ceb159a53b962e1
Dmitriy Robota <dmitriy.robota@gmail.com>
2015-08-22 09:57:44 +0300

    Add library

---
+++ README.md
@@ -0,0 +1,38 @@
+XvfbRobot library for Robot Framework
+==================================================
+
+
+Introduction
+------------
+
+A simple robot library for creating virtual display which can be used for running selenium tests in headless mode, internally uses a [xvfbwrapper](https://pypi.python.org/pypi/xvfbwrapper/0.2.4).
+
+- Information about keywords can be found on the [Keyword Documentation](https://github.com/drobota/robotframework-xvfb/docs/robotframework-xvfb.html) page.
+
+
+Requirements
+------------
+* Python 2.7.3 (Newer versions not tested)
+* Robot Framework 2.8 
+* xvfbwrapper (python library)
+
+Installation
+------------
+#### Using pip ####
+
+The recommended installation tool is [pip](http://pip-installer.org).
+
+Install pip.
+Enter the following:
+
+    pip install robotframework-xvfb
+
+Getting Help
+------------
+The [user group for Robot Framework](http://groups.google.com/group/robotframework-users) is the best place to get help. Include in the post:
+
+- Full description of what you are trying to do and expected outcome
+- Version number of robotframework-xvfb, Robot Framework and xvfbwrapper
+- Traceback or other debug output containing error information
+
+Or create an issue on [github](https://github.com/drobota/robotframework-xvfb)
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+#  Copyright 2015 Dmitriy Robota.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import sys
+from distutils.core import setup
+from os.path import join, dirname
+
+sys.path.append(join(dirname(__file__), "XvfbRobot"))
+
+execfile(join(dirname(__file__), "XvfbRobot", "version.py"))
+
+
+setup(name              = "XfvbRobot",
+      version           = __version__,
+      description       = "Robot library for interacting with Xvfb",
+      long_description  = "XvfbRobot is a robot library which is a wrapper for a xvfbwrapper",
+      author            = "Dmitriy Robota",
+      author_email      = "<dmitriy.robota@gmail.com",
+      url               = "https://github.com/drobota/robotframework-xvfb",
+      license           = "Apache License 2.0",
+      keywords          = "robotframework robot testing testautomation xvfb xvfbwrapper xvfbrobot",
+      platforms         = "any",
+      classifiers       = [
+                              "License :: Apache Software License",
+                              "Programming Language :: Python",
+                              "Intended Audience :: Developers",
+                              "Programming Language :: Python :: 2.7",
+                              "Topic :: Software Development :: Testing",
+                              "Topic :: Software Development :: Quality Assurance"
+                        ],
+      install_requires  = ["robotframework","xvfbwrapper"],
+      packages          = ["XvfbRobot"],
+      download_url      = "https://github.com/drobota/robotframework-xvfb",
+      )
